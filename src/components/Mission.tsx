078b417f947597dead48b2d7039253647acc0b2f
Anusha Tomar <anushatomar913@gmail.com>
2025-06-03 19:32:44 +0530

    cleaned up code

--- src/components/Mission.tsx
+++
@@ -1,9 +0,0 @@
-export default function MissonSection(){
-    return(
-        <div className="mb-8 text-center text-white">
-        AI-powered analysis of misogynistic comments. Try it below.
-
-        </div>
-    )
-}
-
